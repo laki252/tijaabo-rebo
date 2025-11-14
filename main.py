8671ca435cf08fc335f1893e23ffc36a9b4e59be
laki252 <lakicalinuur@icloud.com>
2025-11-14 22:30:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from pymongo import MongoClient
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-TOKEN = "8281896922:AAF5KBeSbSmfA2gb9R_9mLwepM50R-k37GY"
-WEBHOOK_URL = "https://you-tube-save-bot-5tbw.onrender.com"
+TOKEN = "8281896922:AAHgwpFIXFme7NHAIKzt0gQGGQgAst2wjiE"
+WEBHOOK_URL = "https://tijaabo-rebo.onrender.com"
 REQUIRED_CHANNEL = ""
 
 DB_USER = "lakicalinuur"
